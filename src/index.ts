ddd5ae7fbedb3033b799d624f3b8fd785ad2a004
Sunny Hirai <thesunny@gmail.com>
2022-03-29 13:03:45 -0700

    Fix comments

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import dotenv from "dotenv"
-import chalk from "chalk"
-import * as utils from "@thesunny/script-utils"
-import * as s from "superstruct"
-import { setVercelEnv } from "./set-vercel-env"
-import { Options } from "./set-vercel-env"
+#!/usr/bin/env node
+
+import dotenv from "dotenv";
+import chalk from "chalk";
+import * as utils from "@thesunny/script-utils";
+import * as s from "superstruct";
+import { setVercelEnv } from "./set-vercel-env";
+import { Options } from "./set-vercel-env";
+export { setVercelEnv } from "./set-vercel-env";
 
 /**
  * VERCEL DOCS:
@@ -16,10 +19,10 @@ import { Options } from "./set-vercel-env"
  * the environment (production|preview|development) as its second and the
  * gitbranch as a third if in production.
  *
- *   push-vercel-env .env/temporary-test.env production production
- *   push-vercel-env .env/temporary-test.env production staging
- *   push-vercel-env .env/temporary-test.env preview
- *   push-vercel-env .env/temporary-test.env development
+ *   set-vercel-env .env/temporary-test.env production production
+ *   set-vercel-env .env/temporary-test.env production staging
+ *   set-vercel-env .env/temporary-test.env preview
+ *   set-vercel-env .env/temporary-test.env development
  *
  * Every environment variable in the `.env` file will be first removed and then
  * added. If the value of an environment variable is `` (i.e. empty) then the
@@ -41,16 +44,16 @@ import { Options } from "./set-vercel-env"
 const [, , path, environment, gitbranch] = process.argv as (
   | string
   | undefined
-)[]
+)[];
 
-utils.task("Checking CLI arguments")
-utils.message(JSON.stringify(process.argv.slice(2)))
+utils.task("Checking CLI arguments");
+utils.message(JSON.stringify(process.argv.slice(2)));
 
 if (typeof path !== "string") {
-  utils.fail("Expected first CLI arg [path-to-env] to be defined")
+  utils.fail("Expected first CLI arg [path-to-env] to be defined");
 }
 if (!path.endsWith(".env")) {
-  utils.fail("Expected first CLI arg [path-to-env] to end in '.env'")
+  utils.fail("Expected first CLI arg [path-to-env] to end in '.env'");
 }
 
 function checkOptions(
@@ -58,51 +61,51 @@ function checkOptions(
   gitbranch: string | undefined
 ): Options {
   if (typeof environment !== "string") {
-    utils.fail("Expected second CLI arg [environment] to be defined")
+    utils.fail("Expected second CLI arg [environment] to be defined");
   }
   if (!["production", "preview", "development"].includes(environment)) {
     utils.fail(
       `Expected second CLI arg to be production, preview or development but is ${JSON.stringify(
         environment
       )}`
-    )
+    );
   }
-  s.assert(environment, s.enums(["production", "preview", "development"]))
+  s.assert(environment, s.enums(["production", "preview", "development"]));
   if (environment !== "preview" && typeof gitbranch === "string") {
     utils.fail(
       "Third CLI arg cannot be defined expected for in preview environment"
-    )
+    );
   }
-  utils.pass("Done")
+  utils.pass("Done");
   if (environment === "preview") {
-    return { environment, gitbranch }
+    return { environment, gitbranch };
   } else {
-    return { environment }
+    return { environment };
   }
 }
 
-const options = checkOptions(environment, gitbranch)
+const options = checkOptions(environment, gitbranch);
 
-utils.title(`Set environment variables from\n${JSON.stringify(path)}`)
+utils.title(`Set environment variables from\n${JSON.stringify(path)}`);
 
-utils.ensureFileExists(path)
+utils.ensureFileExists(path);
 /**
  * If path wasn't set, show a reminder
  */
 if (typeof path === "undefined") {
-  console.log(chalk.red(`A path argument must be provided but is not`))
-  process.exit(1)
+  console.log(chalk.red(`A path argument must be provided but is not`));
+  process.exit(1);
 }
 
 /**
  * Load env variables using dotenv. Returns `undefined` if a file is not found
  */
-const env = dotenv.config({ path }).parsed
+const env = dotenv.config({ path }).parsed;
 
 if (env === undefined) {
-  console.log(chalk.red(`Could not find .env file at ${JSON.stringify(path)}`))
-  console.log()
-  process.exit(1)
+  console.log(chalk.red(`Could not find .env file at ${JSON.stringify(path)}`));
+  console.log();
+  process.exit(1);
 }
 
-setVercelEnv(env, options)
+setVercelEnv(env, options);
